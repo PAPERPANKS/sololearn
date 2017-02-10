219e56d3cbba2f9035c5ddec7a09275088947863
PAPERPANKS <pnkjgautam05@gmail.com>
2017-02-11 00:23:49 +0530

    sum function added

--- java/myclass.java
+++ java/myclass.java
@@ -1,6 +1,16 @@
 class myclass{
+    int a,b;
+    myclass(){
+        a=b=0;
+    }
+    int print(int a,int b){
+        return a+b;
+    }
     public static void main(String[] args){
         String name="pankaj";
+        myclass sum_obj = new myclass();
+        int value = sum_obj.print(5, 7);
+        System.out.println(value);
         System.out.println("hello "+name+" :) !");
         
     }
